6b0898dbd60f3bdb3d3950e62a83d4d6d0eab3db
Tomasz Kurylik <fulcrumone@icloud.com>
2023-10-08 09:53:35 +0200

    Initial Commit

---
+++ CalendarView Demo/CalendarView_DemoApp.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarView_DemoApp.swift of CalendarView Demo
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+@main
+struct CalendarView_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
