746bf2a7f682c336dd2a3554be13b8543de978e6
Tomasz Kurylik <fulcrumone@icloud.com>
2023-11-24 02:16:13 +0100

    1

--- CalendarView Demo/ContentView.swift
+++ CalendarView Demo/ContentView.swift
@@ -10,16 +10,11 @@
 
 
 import SwiftUI
+import MijickCalendarView
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        MCalendarView(selectedDate: nil, selectedRange: nil)
     }
 }
 
